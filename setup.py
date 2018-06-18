c1c2ad5cd584ba08ac905c1f1beb9f85ad6c3829
Jacob Powers <jpowers@23andme.com>
2018-06-17 19:56:54 -0700

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='primes_times_table',
-    version='0.0.0',
+    version='0.0.1',
     author='Jacob Powers',
     author_email='powersjcb@gmail.com',
     url='https://github.com/powersjcb/primes_times_table',
